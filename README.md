e9e0c5bcefd42dbb6cbda607c8724a55f9d00757
Fernando <fernandorato.0@hotmail.com>
2019-11-10 00:22:52 -0300

    Finalizado aplicação React

---
+++ README.md
@@ -0,0 +1,32 @@
+## Projeto05 - RocketSeat GoStack
+
+
+<h1>Projeto Desenvolvido em ReactJs</h1>
+
+<p>Nesse projeto foram usados algumas libs:</p>
+
+<ul>
+    <li>Axios - Para conexão com API do github</li>
+    <li>Prop-Types - Para validação dos dados passados aos componentes</li>
+    <li>Styled Components - Para fazer a estilização</li>
+    <li>React/React-router-dom - Para fazer o Roteamento das paginas</li>
+    <li>Prettier, eslint -  Para padronizar o codigo</li>
+    <li>entre outras!</li>
+</ul>
+
+
+
+<h1>
+Para executar essa aplicação, faça um clone do repositorio, e execute os comandos:</h1>
+
+```js
+    yarn
+```
+```js
+    yarn start
+```
+
+
+
+<h5>Desenvolvido por Fernando Santos</h5>
+<a href='https://www.linkedin.com/in/fernando-santos-686632122/'>Linkedin</a>
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,23 +1,79 @@
 import React, { Component } from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
+import Container from '../../components/Container';
+import { Form, ButtonSubmit, List, Error } from './styles';
 
-import { Container, Form, ButtonSubmit } from './styles';
-
-class Main extends Component {
+export default class Main extends Component {
     state = {
         newRepo: '',
         repositories: [],
         loading: false,
+        valid: false,
+        errorMessage: '',
     };
+
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories');
+        if (repositories) {
+            this.setState({ repositories: JSON.parse(repositories) });
+        }
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+        if (prevState !== repositories) {
+            localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
 
     handleInputChange = e => {
         this.setState({ newRepo: e.target.value });
-        console.log(this.state.newRepo);
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        this.setState({ loading: true });
+
+        try {
+            const { newRepo, repositories } = this.state;
+            const repoExist = repositories.find(repo => repo.name === newRepo);
+            if (repoExist) {
+                // eslint-disable-next-line no-throw-literal
+                throw 'Repositorio Duplicado';
+            }
+            const response = await api.get(`/repos/${newRepo}`);
+            const data = { name: response.data.full_name };
+
+            this.setState({
+                repositories: [...repositories, data],
+                newRepo: '',
+                loading: false,
+                errorMessage: '',
+                valid: true,
+            });
+        } catch (error) {
+            let message = '';
+            if (error !== 'Repositorio Duplicado') {
+                message = 'Repositorio Inexistente';
+            } else {
+                message = error;
+            }
+            this.setState({ valid: false, newRepo: '', errorMessage: message });
+        }
+        this.setState({ loading: false });
     };
 
     render() {
+        const {
+            newRepo,
+            loading,
+            repositories,
+            valid,
+            errorMessage,
+        } = this.state;
         return (
             <Container>
                 <h1>
@@ -28,16 +84,33 @@ class Main extends Component {
                     <input
                         type="text"
                         placeholder="Adicionar Repositorio"
-                        value={this.state.newRepo}
+                        value={newRepo}
                         onChange={this.handleInputChange}
                     />
-                    <ButtonSubmit disabled>
-                        <FaPlus color="#FFF" size={14} />
+                    <ButtonSubmit loading={loading ? 1 : 0}>
+                        {loading ? (
+                            <FaSpinner color="#fff" size={14} />
+                        ) : (
+                            <FaPlus color="#FFF" size={14} />
+                        )}
                     </ButtonSubmit>
                 </Form>
+                {valid ? <></> : <Error>{errorMessage}</Error>}
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <Link
+                                to={`/repository/${encodeURIComponent(
+                                    repository.name
+                                )}`}
+                            >
+                                Detalhes
+                            </Link>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
 }
-
-export default Main;
